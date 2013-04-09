f67d4f6931da53a27574ca684543604746c5e70e
Victor de Souza Tarelho <victortarelho@hotmail.com>
2013-04-09 01:26:14 -0300

    Primeiro código gerado

---
+++ Gulosa_brigaderia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gulosa_brigaderia;
+
+/**
+ *
+ * @author Victor
+ */
+public class Gulosa_brigaderia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         new Tela_Inicial().setVisible(true);
+    }
+}
